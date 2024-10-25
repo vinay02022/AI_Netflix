d825cbeb91ab23f4c76b45db10abca49418e7bd5
vinay pandey <vinay.work25@gmail.com>
2024-10-25 23:00:59 +0530

    fixed_auth

--- netflix-ai/src/App.jsx
+++ netflix-ai/src/App.jsx
@@ -1,10 +1,11 @@
 import Body from "./components/Body"
-
-
+import {Provider} from  "react-redux"
+import appStore from "./utils/appStore"
 export default function App() {
   return (
-    <>
+    <Provider store={appStore} >
     <Body />
-    </>
+    </Provider>
+
   )
 }
