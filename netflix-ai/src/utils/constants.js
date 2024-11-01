94ce59a7944ac2440ba0e21109ff6aa1fe88d420
vinay pandey <vinay.work25@gmail.com>
2024-11-02 01:20:03 +0530

    TMDB_API_Intregation

--- netflix-ai/src/utils/constants.js
+++ netflix-ai/src/utils/constants.js
@@ -1,2 +1,10 @@
 export const LOGO="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR="https://avatars.githubusercontent.com/u/51539107?v=4&size=64";
+export const TOKEN_MOVIES="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTBmY2NkMDBhMjQ0NjNjYzAxM2IyZjc2ZTRkZDA0MyIsIm5iZiI6MTczMDQ4MTk4NC40ODk4NjU4LCJzdWIiOiI2NzI1MGNiZmM0YWE5ODk0NWVmMzVhYzUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TdY-NNofYbTM2DROPPNOXZiA45980ohljiHG13pCnYk";
+ export const S = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTBmY2NkMDBhMjQ0NjNjYzAxM2IyZjc2ZTRkZDA0MyIsIm5iZiI6MTczMDQ4MjA3Ni42NzUxNzQsInN1YiI6IjY3MjUwY2JmYzRhYTk4OTQ1ZWYzNWFjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tufbLGadjRporv2zh33_Ql346P_PAphDorp8hqH5W-s'
+    }
+  };
