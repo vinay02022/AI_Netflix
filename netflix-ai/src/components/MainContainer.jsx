c823eecaaaedb69a71886426dc49196234ee86f3
vinay pandey <vinay.work25@gmail.com>
2024-11-04 02:42:54 +0530

    movie_main_container

--- netflix-ai/src/components/MainContainer.jsx
+++ netflix-ai/src/components/MainContainer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import VideoTitle from './VideoTitle'
+import VideoBackground from './VideoBackground'
 
 const MainContainer = () => {
+    const movies=useSelector((store)=>{
+        // console.log("mai Store Maccha",store);// abhi store mein saare slice show ho rhe hain lekin hme sirf movies wale ka yha kaam hai
+        store.movies?.nowPlayingMovies
+    })
+    if(!movies) return;//early return
+    const mainMovie=movies[0];
+    console.log("mainMovie",mainMovie);
+    
   return (
     <div>
       MainContainer
+      <VideoTitle />
+      <VideoBackground/>
     </div>
   )
 }
